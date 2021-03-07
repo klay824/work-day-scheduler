683badaec4b7d5de7497313010be0767d361fa36
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-07 16:29:00 -0600

    got the 9am slot background to be gray as it is past 9am

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,17 @@ $("#currentDay").text(currentDay);
 
 // depending on time of day, background of hour will be gray (past), red (current), or green (future)
 var currentTime = moment().hour();
-console.log(currentTime);
+console.log(typeof currentTime);
 
 function timeColors () {
-    if(currentTime >= 4) {
-        $("#input9").addClass("present");
+    if(currentTime > 9) {
+        $("#input9").addClass("past");
+    } else if (currentTime === 9) {
+        $("#input9").addClass("present")
+    } else if (currentTime < 9) {
+        $("#input9").addClass("future")
     }
+    
 }
 
+timeColors();
