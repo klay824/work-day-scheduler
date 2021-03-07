8e39552c963daeb736ad4fa5b2b4f599037499bf
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-07 16:16:00 -0600

    added current date to display, trying to change backgrounds based on time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// global variables
+var saveBtn = document.querySelector(".saveBtn");
+
+
+// current day timestamp
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 
 $("#currentDay").text(currentDay);
+
+// depending on time of day, background of hour will be gray (past), red (current), or green (future)
+var currentTime = moment().hour();
+console.log(currentTime);
+
+function timeColors () {
+    if(currentTime >= 4) {
+        $("#input9").addClass("present");
+    }
+}
+
