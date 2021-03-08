9573e8e8f95c05c7dd712965b7971d3e9da360af
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-07 19:40:00 -0600

    added functions for each time slot to save user input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 // global variables
-var saveBtn = document.querySelector(".saveBtn");
+var saveBtn9 = document.querySelector(".saveBtn9");
+var saveBtn10 = document.querySelector(".saveBtn10");
+var saveBtn11 = document.querySelector(".saveBtn11");
+var saveBtn12 = document.querySelector(".saveBtn12");
+var saveBtn1 = document.querySelector(".saveBtn1");
+var saveBtn2 = document.querySelector(".saveBtn2");
+var saveBtn3 = document.querySelector(".saveBtn3");
+var saveBtn4 = document.querySelector(".saveBtn4");
+var saveBtn5 = document.querySelector(".saveBtn5");
+
+var input9 = document.querySelector("#input9");
+var input10 = document.querySelector("#input10");
+var input11 = document.querySelector("#input11");
+var input12 = document.querySelector("#input12");
+var input1 = document.querySelector("#input1");
+var input2 = document.querySelector("#input2");
+var input3 = document.querySelector("#input3");
+var input4 = document.querySelector("#input4");
+var input5 = document.querySelector("#input5");
+
+var task9 = [];
+var task10 = [];
+var task11 = [];
+var task12 = [];
+var task1 = [];
+var task2 = [];
+var task3 = [];
+var task4 = [];
+var task5 = [];
+
 
 
 // current day timestamp
@@ -148,4 +177,194 @@ function timeColors () {
     }
 }
 
+
+
 timeColors();
+
+
+// 9am saving to local storage
+function save9AM () {
+    
+    localStorage.setItem("task9", JSON.stringify(task9));
+}
+
+saveBtn9.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var taskText9 = input9.value.trim();
+
+    if(taskText9 === "") {
+        return;
+    }
+
+    task9.push(taskText9);
+    input9.value = "";
+
+    
+
+    save9AM();
+    
+    // need help getting this to stay on the page
+    var saveTask9 = JSON.parse(localStorage.getItem("task9"));
+        $("input9").text(saveTask9);
+});
+
+// 10am saving to local storage
+function save10AM () {
+    localStorage.setItem("task10", JSON.stringify(task10));
+}
+
+saveBtn10.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText10 = input10.value.trim();
+
+    if(taskText10 === "") {
+        return;
+    }
+
+    task10.push(taskText10);
+    input10.value = "";
+
+    save10AM();
+})
+
+// 11am saving to local storage
+function save11AM () {
+    localStorage.setItem("task11", JSON.stringify(task11));
+}
+
+saveBtn11.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText11 = input11.value.trim();
+
+    if(taskText11 === "") {
+        return;
+    }
+
+    task11.push(taskText11);
+    input11.value = "";
+
+    save11AM();
+})
+
+// 12pm saving to local storage
+function save12PM () {
+    localStorage.setItem("task11", JSON.stringify(task12));
+}
+
+saveBtn12.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText12 = input12.value.trim();
+
+    if(taskText12 === "") {
+        return;
+    }
+
+    task12.push(taskText12);
+    input12.value = "";
+
+    save12PM();
+})
+
+// 1pm saving to local storage
+function save1PM () {
+    localStorage.setItem("task1", JSON.stringify(task1));
+}
+
+saveBtn1.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText1 = input1.value.trim();
+
+    if(taskText1 === "") {
+        return;
+    }
+
+    task1.push(taskText1);
+    input1.value = "";
+
+    save1PM();
+})
+
+// 2pm saving to local storage
+function save2PM () {
+    localStorage.setItem("task2", JSON.stringify(task2));
+}
+
+saveBtn2.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText2 = input2.value.trim();
+
+    if(taskText2 === "") {
+        return;
+    }
+
+    task2.push(taskText2);
+    input2.value = "";
+
+    save2PM();
+})
+
+// 3pm saving to local storage
+function save3PM () {
+    localStorage.setItem("task3", JSON.stringify(task3));
+}
+
+saveBtn3.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText3 = input3.value.trim();
+
+    if(taskText3 === "") {
+        return;
+    }
+
+    task3.push(taskText3);
+    input3.value = "";
+
+    save3PM();
+})
+
+// 4pm saving to local storage
+function save4PM () {
+    localStorage.setItem("task4", JSON.stringify(task4));
+}
+
+saveBtn4.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText4 = input4.value.trim();
+
+    if(taskText4 === "") {
+        return;
+    }
+
+    task4.push(taskText4);
+    input4.value = "";
+
+    save4PM();
+})
+
+// 5pm saving to local storage
+function save5PM () {
+    localStorage.setItem("task5", JSON.stringify(task5));
+}
+
+saveBtn5.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var taskText5 = input5.value.trim();
+
+    if(taskText5 === "") {
+        return;
+    }
+
+    task5.push(taskText5);
+    input5.value = "";
+
+    save5PM();
+})
