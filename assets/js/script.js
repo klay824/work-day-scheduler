a0a32cb9779d8e892888d79f41a125651584095c
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-07 18:16:15 -0600

    added conditional statements for each time of day and color

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,143 @@ $("#currentDay").text(currentDay);
 
 // depending on time of day, background of hour will be gray (past), red (current), or green (future)
 var currentTime = moment().hour();
-console.log(typeof currentTime);
 
+// function to change textarea background color depending on time of day
 function timeColors () {
+    // gray if 9am is in the past
     if(currentTime > 9) {
         $("#input9").addClass("past");
-    } else if (currentTime === 9) {
-        $("#input9").addClass("present")
-    } else if (currentTime < 9) {
-        $("#input9").addClass("future")
-    }
+    } 
     
+    // red if the current hour is 9am
+    if (currentTime == 9) {
+        $("#input9").addClass("present");
+    } 
+    
+    // green if 9am is in the future
+    if (currentTime < 9) {
+        $("#input9").addClass("future");
+    }
+
+    // gray if 10am is in the past
+    if(currentTime > 10) {
+        $("#input10").addClass("past");
+    }
+
+    // red if 10am is the current hour
+    if(currentTime == 10) {
+        $("#input10").addClass("present");
+    }
+
+    // green if 10am is in the future
+    if(currentTime < 10) {
+        $("#input10").addClass("future");
+    }
+
+    // gray if 11am is in the past
+    if(currentTime > 11) {
+        $("#input11").addClass("past");
+    }
+
+    // red if 11am is the current hour
+    if(currentTime == 11) {
+        $("#input11").addClass("present");
+    }
+
+    // green if 11am is in the future
+    if(currentTime < 11) {
+        $("#input11").addClass("future");
+    }
+
+    // gray if 12pm is in the past
+    if(currentTime > 12) {
+        $("#input12").addClass("past");
+    }
+
+    // red if 12pm is the current time
+    if(currentTime == 12) {
+        $("#input12").addClass("present");
+    }
+
+    // green if 12pm is in the future
+    if(currentTime < 12) {
+        $("#input12").addClass("future");
+    }
+
+    // gray if 1pm is in the past
+    if(currentTime > 13) { /*13 because this is a 24 hour clock */
+        $("#input1").addClass("past");
+    }
+
+    // red if 1pm is the current hour
+    if(currentTime == 13) {
+        $("#input1").addClass("present");
+    }
+
+    // green if 1pm is in the future
+    if(currentTime < 13) {
+        $("#input1").addClass("future");
+    }
+
+    // gray if 2pm is in the past
+    if(currentTime > 14) {
+        $("#input2").addClass("past");
+    }
+
+    // red if 2pm is the current hour
+    if(currentTime == 14) {
+        $("#input2").addClass("present");
+    }
+
+    // green if 2pm is in the future
+    if(currentTime < 14) {
+        $("#input2").addClass("future");
+    }
+
+    // gray if 3pm is in the past
+    if(currentTime > 15) {
+        $("#input3").addClass("past");
+    }
+
+    // red if 3pm is the current hour
+    if(currentTime == 15) {
+        $("#input3").addClass("present");
+    }
+
+    // green if 3pm is in the future
+    if(currentTime < 15) {
+        $("#input3").addClass("future");
+    }
+
+    // gray if 4pm is in the past
+    if(currentTime > 16) {
+        $("#input4").addClass("past");
+    }
+
+    // red if 4pm is the current hour
+    if(currentTime == 16) {
+        $("#input4").addClass("present");
+    }
+
+    // green if 4pm is in the future
+    if(currentTime < 16) {
+        $("#input4").addClass("future");
+    }
+
+    // gray if 5pm is in the past
+    if(currentTime > 17) {
+        $("#input5").addClass("past");
+    }
+
+    // red if 5pm is the current hour
+    if(currentTime == 17) {
+        $("#input5").addClass("present");
+    }
+
+    // green if 5pm is in the future
+    if(currentTime < 17) {
+        $("#input5").addClass("future");
+    }
 }
 
 timeColors();
